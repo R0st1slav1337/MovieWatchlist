6c3e39127b3cd58317d2b7ef42354a5090e43caa
Rostislav <r_krivenko@kbtu.kz>
2026-04-01 19:46:17 +0500

    upd

--- GameTracker/src/app/app.ts
+++ GameTracker/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('MovieWatchlist');
+  protected readonly title = signal('GameTracker');
 }
